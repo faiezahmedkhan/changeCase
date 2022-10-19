a29acb7990c9b4673f7c93c53dd0796ef5fe04f0
unknown <faiez844@gmail.com>
2022-10-19 21:34:49 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,9 @@
 import React from 'react'
-import Navbar  from './component/navbar'
 import Textform from './component/TextForm'
 
 const App = () => {
   return (
     <>
-<Navbar title="MyNavbar"/>
 <div className="container my-3">
 <Textform heading="Enter the text to analyze below"/>
 </div>
--- src/component/TextForm.js
+++ src/component/TextForm.js
@@ -10,7 +10,29 @@ const TextForm = (props) => {
         setText(newText);
 
     }
-    const handelOnChange = (e) =>{
+
+    const handelclicklower = () =>{
+      console.log('UpperCase Was Clicked');
+      let newText = text.toLowerCase();
+      setText(newText);
+
+  }
+
+
+  const handelcear = () =>{
+    let newText = '';
+    setText(newText);
+
+}
+
+const handerCopy = () =>{
+var text = document.getElementById("myBox")
+  text.select()
+  navigator.clipboard.writeText(text.value);
+
+}
+
+  const handelOnChange = (e) =>{
         setText(e.target.value);
 
     }
@@ -25,7 +47,24 @@ const TextForm = (props) => {
                 <textarea className="form-control" value={text} onChange={handelOnChange}  id="myBox" rows="8"></textarea>
                 </div>
         
-               <button className='btn btn-success' onClick={handelclick}>Convert to Uppercase</button>      
+               <button className='btn btn-success mx-1' onClick={handelclick}>Convert to Uppercase</button>
+               <button className='btn btn-success mx-1' onClick={handelclicklower}>Convert to Lowercase</button>
+               <button className='btn btn-success mx-1' onClick={handelcear}>Clear</button>   
+               <button className='btn btn-success mx-1' onClick={handerCopy}>Copy to Clipboard</button>   
+          
+
+
+
+
+               <div className="container my-4">
+                <h1>Your Text Summery</h1>
+                <p>{text.split(" ").length} words and {text.length}</p>
+                <p>{0.888 *  text.split(" ").length} Minuts Read</p>
+
+                <h2>Plain Text Here</h2>
+                <p>{text}</p>
+
+               </div> 
 
     </>
   )
